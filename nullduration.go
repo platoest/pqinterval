5c99aed0fb8bdb5e579cdbdf928663ea5070c556
Prasanna Swaminathan <prasanna@ergatta.com>
2019-06-05 21:57:19 -0400

    Add JSON marshaling for NullDuration

--- nullduration.go
+++ nullduration.go
@@ -2,6 +2,9 @@ package pqinterval
 
 import (
 	"database/sql/driver"
+	"encoding/json"
+	"errors"
+	"time"
 )
 
 type NullDuration struct {
@@ -39,3 +42,40 @@ func (nd NullDuration) Value() (driver.Value, error) {
 }
 
 var _ driver.Valuer = NullDuration{}
+
+// MarshalJSON implements json.Marshaler
+func (nd NullDuration) MarshalJSON() ([]byte, error) {
+	if !nd.Valid {
+		return []byte("null"), nil
+	}
+	return json.Marshal(time.Duration(nd.Duration).String())
+}
+
+// UnmarshalJSON implements json.Unmarshaler
+func (nd *NullDuration) UnmarshalJSON(b []byte) error {
+	var v interface{}
+	if err := json.Unmarshal(b, &v); err != nil {
+		return err
+	}
+	switch value := v.(type) {
+	case float64:
+		nd.Valid = true
+		nd.Duration = Duration(time.Duration(value))
+		return nil
+	case string:
+		tmp, err := time.ParseDuration(value)
+		if err != nil {
+			nd.Valid = false
+			return err
+		}
+
+		nd.Valid = true
+		nd.Duration = Duration(tmp)
+		return nil
+	case nil:
+		nd.Valid = false
+		return nil
+	default:
+		return errors.New("invalid duration")
+	}
+}
